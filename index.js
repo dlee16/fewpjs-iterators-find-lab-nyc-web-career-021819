12e6d6f7a7572e8bb08eb8094fadf9edc3d77024
dlee16 <diana.lee116@gmail.com>
2019-04-11 17:46:50 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let singleRec = record.find(rec => rec.result === "W")
+  if (singleRec) {
+    return singleRec.year
+  } else {
+    return undefined
+  }
+}
